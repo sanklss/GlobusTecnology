a3dd07fa629aafbb35e340d90d10fcb008082c82
sanklss <sanklsswt2@yandex.ru>
2026-01-27 16:16:43 +0300

    Adding Converter and  Auth Service

---
+++ Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using GlobusT.Data;
+using GlobusT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobusT.Services
+{
+    public class AuthService
+    {
+        public Role TryAuth(string username, string password)
+        {
+            using (var context = new GlobusTechnologyContext())
+            {
+                User user = context.Users.FirstOrDefault(u => u.Login == username & u.Password == password);
+
+                if (user == null)
+                {
+                    return null;
+                }
+
+                return context.Roles.FirstOrDefault(r => r.Id == user.IdRole);
+            }
+        }
+    }
+}
